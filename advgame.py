2ed1e5bdc8ea7cb2de679e77746c0d9d12dccdaa
Charles Mogayzel <mogayzelc@gmail.com>
2022-01-24 11:01:37 -0800

    Added new code.

--- advgame.py
+++ advgame.py
@@ -14,12 +14,22 @@ def intro(item, option):
     pause_print("You find your self in the middle of a desert, it is hot and no shade or water")
     pause_print("You see something in the distant, but don't know if it is real or not.")
     pause_print("You are a " + option + " .")
+    
+
+def mirage(option2):
+    if (option2):
+        pause_print("You find a small body of water to quench your thirst")
+        pause_print("You get a drink of water and continue walking past the cave")
+    else:
+        pause_print("Afer for walking for hours or days you learn it was just a marage and there is no water.")
+        pause_print("You continue walking and find a cave")
 
 
 def gamePlay():
     item = []
     option = random.choice(["soldier","merchent","wanderer","camel"])
     intro(item,option)
+
 
 
 gamePlay()
