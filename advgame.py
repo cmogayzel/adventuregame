9712ade4cc1c93ce5a27b7956756c2a513761d7c
Charles Mogayzel <mogayzelc@gmail.com>
2022-01-24 16:14:55 -0800

    Update advgame.py
    Added new lines of code. Expanding adventure game.

--- advgame.py
+++ advgame.py
@@ -11,23 +11,47 @@ def pause_print(printMessage):
 
 
 def intro(item, option):
-    pause_print("You find your self in the middle of a desert, it is hot and no shade or water")
-    pause_print("You see something in the distant, but don't know if it is real or not.")
+    pause_print("You find your self in the forest in the middle of winter")
+    pause_print("You see a cabin in the distances but not sure if someone is home or not.")
     pause_print("You are a " + option + " .")
     
 
-def mirage(option2):
-    if (option2):
-        pause_print("You find a small body of water to quench your thirst")
-        pause_print("You get a drink of water and continue walking past the cave")
+def forest(item, option):
+    pause_print("You are approaching the door of the cabin.\n")
+    pause_print("What do you do.")
+    house(item,option)
+    
+
+
+def  house(item,option):
+    pause_print("Enter 1 to enter cabin.")
+    pause_print("Enter 2 to peer into cabin.")
+    pause_print("What would you like to do?")
+    while True:
+        choice = input("(Please enter 1 or 2.)\n")
+        if choice == "1":
+            break
+        elif choice == "2":
+            break
+        else:
+            pause_print("Error, you did choose 1 or 2")
+            play_again()
+            break
+
+def play_again():
+    again = input("Would you like to play again? (y/n)").lower()
+    if again == "y" or again == "Y":
+        pause_print("\n\n\nExcellent! Restarting the game ...\n\n\n")
+        pause_print()
+    elif again == "n" or again == "N" :
+        pause_print("\n\n\nThanks for playing!\n\n\n")
     else:
-        pause_print("Afer for walking for hours or days you learn it was just a marage and there is no water.")
-        pause_print("You continue walking and find a cave")
+        play_again()
 
 
 def gamePlay():
     item = []
-    option = random.choice(["soldier","merchent","wanderer","camel"])
+    option = random.choice(["soldier","hunter","survivalist","park ranger"])
     intro(item,option)
 
 
