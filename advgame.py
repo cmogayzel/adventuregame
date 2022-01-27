97f6360e38d46b8d0e2f905ed6ee10913e4cf7fa
Charles Mogayzel <mogayzelc@gmail.com>
2022-01-26 21:17:37 -0800

    Added code to the project.

--- advgame.py
+++ advgame.py
@@ -11,30 +11,59 @@ def pause_print(printMessage):
 
 
 def intro(item, option):
-    pause_print("You find your self in the forest in the middle of winter")
-    pause_print("You see a cabin in the distances but not sure if someone is home or not.")
+    pause_print("You are walking through the forsest and come up on a old cabin.\n")
+    pause_print("You notice the cabin is very old and looks abandond.")
+    pause_print("You are very cold and hungry and the cabin would provide shelter from the cold ")
+    pause_print("and could have food and firewood to stay warm, but your guts tell you there is something wrong.")
+    pause_print("What do you do?")
     pause_print("You are a " + option + " .")
-    
 
-def forest(item, option):
-    pause_print("You are approaching the door of the cabin.\n")
-    pause_print("What do you do.")
-    house(item,option)
-    
+def zombieSuprise():
+         pause_print("You enter the cabin and are surpised by a zombie.")
+         pause_print("You are killed by the zombie.")
+         pause_print("The end")
+       
+
+def zombieAttack(randomItem,options): 
+       pause_print("You enter the cabin with your " + randomItem + " and attack the zombie.")
+       if randomItem == "stick":
+           pause_print("You are killed by the zombie " + randomItem + " was not enough to defeat the zombie.")
+           pause_print("The end")
+       elif randomItem == "axe":
+            pause_print("Your " + randomItem + " defeats the zombine and you have enough food and firewood to survive winter.")
+            
+
+def walkaround():
+     pause_print("You walk around the cabin to find there is a zombie in the cabin.")
+     pause_print("what do you do?")
+     pause_print("Enter 1 to enter cabin.")
+     pause_print("Enter 2 walk away and find another cabin.")
+     choice = input("Enter 1 or 2 ")
+     if choice == "1":
+         zombieAttack()
+     elif choice == "2":
+         pause_print("You chose to move on and find another cabin that doesn't a zombie in it.")
+         pause_print("Your choice causes you to freeze and not find another cabin.")
+         pause_print("The end.")
 
 
-def  house(item,option):
+
+def cabin(randomItem,option):
     pause_print("Enter 1 to enter cabin.")
     pause_print("Enter 2 to peer into cabin.")
-    pause_print("What would you like to do?")
+    pause_print("What would you like to do? ")
     while True:
-        choice = input("(Please enter 1 or 2.)\n")
+        choice = input("(Please enter 1 or 2 )")
         if choice == "1":
+            zombieSuprise()
+            play_again()
             break
         elif choice == "2":
+            walkaround()
+            play_again()
             break
         else:
-            pause_print("Error, you did choose 1 or 2")
+            pause_print("Error, you did choose 1 or 2. ")
             play_again()
             break
 
@@ -50,10 +79,10 @@ def play_again():
 
 
 def gamePlay():
-    item = []
+    randomItem = random.choice(["stick","axe"])
     option = random.choice(["soldier","hunter","survivalist","park ranger"])
-    intro(item,option)
-
+    intro(randomItem,option)
+    cabin(randomItem,option)
 
 
 gamePlay()
